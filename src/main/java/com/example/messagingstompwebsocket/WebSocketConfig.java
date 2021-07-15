aa50cb2fd61cdc3cc9595769f47d07661e510257
kna-github <nikolay.kuznetsov@smddev.com>
2021-07-16 00:41:35 +0300

    interceptor added

--- src/main/java/com/example/messagingstompwebsocket/WebSocketConfig.java
+++ src/main/java/com/example/messagingstompwebsocket/WebSocketConfig.java
@@ -1,14 +1,45 @@
 package com.example.messagingstompwebsocket;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.messaging.support.MessageHeaderAccessor;
+import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.server.HandshakeInterceptor;
+
+import java.util.Map;
 
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+	Logger logger = LoggerFactory.getLogger(WebSocketConfig.class);
+
+	@Override
+	public void configureClientInboundChannel(ChannelRegistration registration) {
+		registration.interceptors(new ChannelInterceptor() {
+			@Override
+			public Message<?> preSend(Message<?> message, MessageChannel channel) {
+				StompHeaderAccessor accessor =
+						MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
+				if (StompCommand.CONNECT.equals(accessor.getCommand())) {
+					logger.info("connection message " + accessor.getMessageHeaders().toString());
+				}
+				return message;
+			}
+		});
+	}
 
 	@Override
 	public void configureMessageBroker(MessageBrokerRegistry config) {
@@ -18,7 +49,25 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
-		registry.addEndpoint("/gs-guide-websocket").setAllowedOriginPatterns("*").withSockJS();
+		registry.addEndpoint("/gs-guide-websocket")
+				.setAllowedOriginPatterns("*")
+				.addInterceptors(getInterceptor())
+				.withSockJS();
+	}
+
+	private HandshakeInterceptor getInterceptor() {
+		return new HandshakeInterceptor(){
+
+			@Override
+			public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Map<String, Object> attributes) throws Exception {
+				logger.info("Before Handshake " + request.getHeaders().toString());
+				return true; //TODO
+			}
+
+			@Override
+			public void afterHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Exception exception) {}
+
+		};
 	}
 
 }
